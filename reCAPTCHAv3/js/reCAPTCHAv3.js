4510cd579713a8abc8ad5d156a697d41b6375c3c
WestleyBennett <50267102+WestleyBennett@users.noreply.github.com>
2021-08-17 11:35:39 -0500

    Dealing with Token Expiration
    Updated to deal with the 120 second timeout on Google's Token

--- reCAPTCHAv3/js/reCAPTCHAv3.js
+++ reCAPTCHAv3/js/reCAPTCHAv3.js
@@ -1,29 +1,36 @@
-//Submit token to the Token variable on page load. The first value there is the site key
-grecaptcha.ready(function () {
-    //Insert your site key here within the quotation marks right after the execute method name
-    grecaptcha.execute('SiteKey', { action: 'homepage' }).then(function (token) {
+getReCaptcha();  // This is the initial call to request the initial token
 
-        $.ajax({
-            type: "POST",
-            url: "/reCAPTCHAv3Service.asmx/SetToken",
-            data: JSON.stringify({ _token: token }),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (response) {
-                console.log('Passed the token successfully');
-            },
-            failure: function (response) {
-                document.getElementById("reCAPTCHAResponse").value = response.d;
-            },
-            error: function (err) {
-                document.getElementById("reCAPTCHAResponse").value = JSON.stringify(err, null, 2);
-            }
+ //This sets the interval to keep requesting a new token every 90 seconds, as Google's tokens expire after 120 seconds
+setInterval(function () { getReCaptcha(); }, 90 * 1000);
+
+function getReCaptcha() {
+    grecaptcha.ready(function () {
+        //Insert your site key here within the quotation marks right after the execute method name
+        grecaptcha.execute('SiteKey', { action: 'homepage' }).then(function (token) {
+
+            $.ajax({
+                type: "POST",
+                url: "/reCAPTCHAv3Service.asmx/SetToken",
+                data: JSON.stringify({ _token: token }),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    console.log('Passed the token successfully');
+                },
+                failure: function (response) {
+                    document.getElementById("reCAPTCHAResponse").value = response.d;
+                },
+                error: function (err) {
+                    document.getElementById("reCAPTCHAResponse").value = JSON.stringify(err, null, 2);
+                }
+            });
         });
     });
-});
+ }
 
 //Add Button trigger - sets the return value back to the hidden reCAPTCHAResponse field
 function btnClick() {
+    //Now post to verify the Captcha
     $.ajax({
         type: "POST",
         async: false,
